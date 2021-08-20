8dc550fee3bccec4bfe02597987ce42d88059ee6
JesusMeister <88863350+JesusMeister@users.noreply.github.com>
2021-08-20 17:54:30 -0500

    Update exercise.py

--- assignments/02AreaTriangulo/src/exercise.py
+++ assignments/02AreaTriangulo/src/exercise.py
@@ -1,6 +1,9 @@
+
 def main():
     #escribe tu código abajo de esta línea
-    
+    b=float(input("Base del traingulo: "))
+    h=float(input("Altura del traingulo: "))
+    area=b*h/2
     print("El área es:",area)
 
 
